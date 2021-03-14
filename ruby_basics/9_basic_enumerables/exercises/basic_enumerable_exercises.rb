cfb2078527a26d763d6c0e4ac84249257170ed1f
Kalon Hinds <kjhinds@gmail.com>
2021-03-13 19:17:09 -0800

    Completed basic enumerator exercises

--- ruby_basics/9_basic_enumerables/exercises/basic_enumerable_exercises.rb
+++ ruby_basics/9_basic_enumerables/exercises/basic_enumerable_exercises.rb
@@ -1,22 +1,26 @@
 def display_current_inventory(inventory_list)
   # use #each to iterate through each item of the inventory_list (a hash)
   # use puts to output each list item "<key>, quantity: <value>" to console
+  inventory_list.each {|key, value| puts "#{key}, quantity: #{value}"}
 end
 
 def display_guess_order(guesses)
   # use #each_with_index to iterate through each item of the guesses (an array)
   # use puts to output each list item "Guess #<number> is <item>" to console
   # hint: the number should start with 1
+  guesses.each_with_index {|guess,index| puts "Guess ##{index+1} is #{guess}"}
 end
 
 def find_absolute_values(numbers)
   # use #map to iterate through each item of the numbers (an array)
   # return an array of absolute values of each number
+  numbers.map {|number| number.abs}
 end
 
 def find_low_inventory(inventory_list)
   # use #select to iterate through each item of the inventory_list (a hash)
   # return a hash of items with values less than 4
+  inventory_list.select {|item,value| value < 4}
 end
 
 def find_longest_word(word_list)
@@ -24,16 +28,31 @@ def find_longest_word(word_list)
   # return the longest word in the word_list
   # hint: the result of each iteration should be the accumulator when its 
   # length is greater than word.length (otherwise the result should be the word)
+  word_list.reduce do |longest, word|
+    if longest.length > word.length
+      longest
+    else
+      word
+    end
+  end
 end
 
 def find_longer_words(word_list, base_word)
   # use #reduce to iterate through each item of the word_list (an array)
   # return an array of words that is longer than the base_word (a string)
   # hint: use a new array or an empty array as the initial accumulator value
+  word_list.reduce([]) do |array, word|
+    array.push(word) if word.length > base_word.length
+    array
+  end
 end
 
 def find_word_lengths(word_list)
   # use #reduce to iterate through each item of the word_list (an array)
   # return a hash with each word as the key and its length as the value
   # hint: use a new hash or an empty hash as the initial accumulator value
+  word_list.reduce(Hash.new(0)) do |array,word|
+    array[word] = word.length
+    array
+  end
 end
