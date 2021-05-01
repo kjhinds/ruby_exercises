21a78cc202687eed2254dda7cc3f134f630854c4
Kalon Hinds <kjhinds@gmail.com>
2021-05-01 15:29:25 -0700

    Add delete and level_order methods to binary search tree

--- binary_search_tree/lib/tree.rb
+++ binary_search_tree/lib/tree.rb
@@ -8,7 +8,7 @@ class Tree
   end
 
   def build_tree(array)
-    return nil if array.nil?
+    return nil if array.empty?
 
     midpoint = (array.length / 2).round
     root_node = Node.new(array[midpoint])
@@ -33,18 +33,48 @@ class Tree
     if root_node.nil?
       Node.new(value)
     else
-      if root_node.value == value
-        root_node
-      elsif root_node.value < value
+      if root_node.value < value
         root_node.right = insert(root_node.right, value)
-      else
+      elsif root_node.value > value
         root_node.left = insert(root_node.left, value)
       end
       root_node
     end
   end
 
-  def delete(value)
-    
+  def delete(root_node, value)
+    return root_node if root_node.nil?
+
+    if value < root_node.value
+      root_node.left = delete(root_node.left, value)
+    elsif value > root_node.value
+      root_node.right = delete(root_node.right, value)
+    else
+      return node.right if root_node.left.nil?
+      return node.left if root_node.right.nil?
+
+      temp = min_value_node(root_node.right)
+      root_node.value = temp.value
+      root_node.right = delete(root_node.right, temp.value)
+    end
+    root_node
+  end
+
+  def min_value_node(node)
+    node = node.left until node.left.nil?
+    node
+  end
+
+  def level_order(root_node)
+    array = []
+    queue = []
+    current_node = root_node
+    until current_node.nil? do
+      array.push(current_node.value)
+      queue.push(current_node.left) unless current_node.left.nil?
+      queue.push(current_node.right) unless current_node.right.nil?
+      current_node = queue.shift
+    end
+    array
   end
 end
